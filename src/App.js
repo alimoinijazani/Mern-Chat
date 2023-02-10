821e5c27fd7fa8d6d100bc486aebb673b590ea2c
alimoinijazani <Ali_28848>
2023-02-10 10:57:23 +0330

    basic routes and navigation

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Container from 'react-bootstrap/Container';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Navigation from './components/Navigation';
+import Chat from './pages/Chat';
 function App() {
-  return <div>app</div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <Navigation />
+        <Container>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/chat" element={<Chat />} />
+          </Routes>
+        </Container>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Navigation.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import Container from 'react-bootstrap/Container';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import { FaRocketchat } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
+import { LinkContainer } from 'react-router-bootstrap';
+export default function Navigation() {
+  return (
+    <Navbar bg="light">
+      <Container>
+        <LinkContainer to="/">
+          <Navbar.Brand>
+            Family Chat
+            <FaRocketchat />
+          </Navbar.Brand>
+        </LinkContainer>
+        <Nav className="ms-auto d-flex justify-content-center align-items-center">
+          <Nav.Item>
+            <Link to="/chat">Chat</Link>
+          </Nav.Item>
+
+          <NavDropdown title="dropdown">
+            <NavDropdown.Item>ppppppppppp</NavDropdown.Item>
+          </NavDropdown>
+        </Nav>
+      </Container>
+    </Navbar>
+  );
+}
